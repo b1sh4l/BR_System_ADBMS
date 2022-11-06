cd3af04ff870772c2d28db3f0daa7a59e6ee5d5f
Bishal Sudarshan <sudarshansahabishal@gmail.com>
2022-11-06 18:19:30 +0600

    Login and Registration Web Pages with Proper Authentication

---
+++ Web/View/Dashboard.php
@@ -0,0 +1,22 @@
+<?php
+include ("../Controller/SignInCheck.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/Header.css">
+    <title></title>
+</head>
+<body>
+<h2 align="center"><b><u>WELCOME!<?php $wfname ?></u></b></h2>
+
+<div>
+    <ul>
+        <li><a href="../Controller/SignOut.php">SignOut</a></li>
+    </ul>
+</div>
+</body>
+</html>
